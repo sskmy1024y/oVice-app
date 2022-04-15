0fa0f64f0da53dbdcd875a3062d5b0dce15d82d5
sho <sskmy1024.y@gmail.com>
2022-04-16 00:43:18 +0900

    feat: ✨ support darkmode

--- src/renderer/screencapture/main.js
+++ src/renderer/screencapture/main.js
@@ -11,29 +11,24 @@ document.onkeydown = function (evt) {
  * @return {Element}
  */
 function htmlToElement(html) {
-  var template = document.createElement("template");
+  const template = document.createElement("template");
   html = html.trim(); // Never return a text node of whitespace as the result
   template.innerHTML = html;
   return template.content.firstChild;
 }
 
 window.electronAPI.getSources(function (event, sources) {
-  let sourcesList = document.querySelector(".capturer-list");
-  for (let source of sources) {
-    let thumb = source.thumbnail;
-    let title = source.name.slice(0, 20);
-    let item = `<li><a href="#"><img src="${thumb}"><span>${title}</span></a></li>`;
-    sourcesList.appendChild(htmlToElement(item));
-  }
-  let links = sourcesList.querySelectorAll("a");
-  for (let i = 0; i < links.length; ++i) {
-    let closure = (i) => {
-      return (e) => {
-        e.preventDefault();
-        window.electronAPI.sourceIdSelected(sources[i].id);
-        sourcesList.innerHTML = "";
-      };
+  const sourcesList = document.querySelector(".capturer-list");
+  sources.forEach((source) => {
+    const thumb = source.thumbnail;
+    const title = source.name.slice(0, 20);
+    const item = `<button><img src="${thumb}"><span>${title}</span></button>`;
+    const element = htmlToElement(item);
+    element.onclick = (e) => {
+      e.preventDefault();
+      window.electronAPI.sourceIdSelected(source.id);
+      sourcesList.innerHTML = "";
     };
-    links[i].onclick = closure(i);
-  }
+    sourcesList.appendChild(element);
+  });
 });
